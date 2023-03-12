2f76d29565c8459d18121fa95992dc7c9cd3d617
Mark Kenneth Roxas <roxas.markkenneth29@gmail.com>
2023-03-12 11:27:52 +0800

    Mp1 to Mp2

---
+++ MP1-MP2/carousel.js
@@ -0,0 +1,7 @@
+// function init_carousel() {
+//     H = +($(window).height() 300px);
+//     W = +($(window).width() 300px);
+//     $('.carousel-inner').css('height', H + 'px');
+// }
+// window.onload = init_carousel;
+// init_carousel();
--- carousel.js
+++
@@ -1,7 +0,0 @@
-function init_carousel() {
-    H = +($(window).height() 300px); // or $('.carousel-inner') as you want ...
-    W = +($(window).width() 300px);
-    $('.carousel-inner').css('height', H + 'px');
-}
-window.onload = init_carousel;
-init_carousel();
